c2c9e3962f5b5ba469fdcdaf9352db7a965b4484
Łukasz Buczyński <lukasz.a.buczynski@gmail.com>
2026-09-05 04:43:08 +0200

    Remove unused function.

--- src/User.ts
+++ src/User.ts
@@ -1,21 +1,6 @@
 
 import NDK, { NDKUser } from "@nostr-dev-kit/ndk";
 
-function DisplayUserInfo(user: NDKUser | null) : void {
-    const UserHTML = document.getElementById("LoggedUser")
-    const NickHtml = document.getElementById("LoggedUserNick")
-    const PubkeyHtml = document.getElementById("LoggedUserPubkey")
-    if (user) {
-        UserHTML.style.display = "block"
-        NickHtml.innerText = user.profile?.name || user.profile?.displayName || ""
-        PubkeyHtml.innerText = user.pubkey
-    } else {
-        UserHTML.style.display = "none"
-        NickHtml.innerText = ""
-        PubkeyHtml.innerText = ""
-    }
-}
-
 export class User {
     ndk: NDK
 
@@ -37,28 +22,4 @@ export class User {
         }
         return user
     }
-}
-
-export async function UpdateUserInfo(ndk: NDK) : Promise<void> {
-    let user : NDKUser | null = null
-    if (ndk.signer) {
-        user = await ndk.signer.user()
-        if (user) {
-            console.log("Fetching profile...")
-            user.fetchProfile({
-                closeOnEose: true,
-                groupable: false
-            }).then((profile) => {/*
-                console.log("Printing profile...")
-                if (user?.profile) {
-                    console.log("Pełny obiekt profilu:", user.profile);
-  
-                    Object.entries(user.profile).forEach(([key, value]) => {
-                        console.log(`${key}:`, value);
-                    });
-                }*/
-            })
-        }
-    }
-    DisplayUserInfo(user)
 }
