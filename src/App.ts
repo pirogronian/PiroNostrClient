b59cf81b68c12a3a235639ea16dd31cdb313d18b
Łukasz Buczyński <lukasz.a.buczynski@gmail.com>
2026-09-14 22:37:31 +0200

    Extend cache size for events.

--- src/App.ts
+++ src/App.ts
@@ -30,7 +30,7 @@ export class App extends Module {
         App._app = this
         this.router = new Router()
         console.log("Router:", this.router)
-        const cacheAdapter = new NDKCacheAdapterDexie({ dbName: 'wiki-nostr-cache' });
+        const cacheAdapter = new NDKCacheAdapterDexie({ dbName: 'wiki-nostr-cache', eventCacheSize: 10000 });
         this.ndk = new NDK({ cacheAdapter, enableOutboxModel: true });
         this.about = new About()
         this.about.register("about", "about", this)
